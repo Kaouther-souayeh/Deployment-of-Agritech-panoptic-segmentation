0c0093558daf54d29986ed33cf05567aeb4c4fb8
Kaouther-souayeh <kaouther.souayeh@nextav.ai>
2023-05-17 14:02:45 +0100

    terraform code

---
+++ terraformCode/modules/keyvault/main.tf
@@ -0,0 +1,32 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = var.keyvault_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create" ,
+      "List",
+      "Get" 
+    ]
+
+    secret_permissions = [
+      "Set",
+      "List",
+      "Get" ,
+      "Delete" ,
+      "Purge",
+      "Recover"
+    ] 
+  }
+  
+ 
+}
